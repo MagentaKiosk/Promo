56ee0c8c4c28fdf67302e80185dea0bfeaf1df4c
MagentaKiosk <165415716+MagentaKiosk@users.noreply.github.com>
2024-03-30 16:02:08 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
         {children}
 
 
-        <ContactMe />
+        <Suspense>
+          <ContactMe />
+        </Suspense>
       </body>
     </html>
   );
